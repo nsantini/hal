716f2b8cb910cff92af47676fffc89aea7bc4bab
Nicolas Santini <nicolas.santini@findly.com>
2015-05-21 20:14:29 +1200

    Setting up framework

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var reactify = require('reactify');
+var nodemon = require('gulp-nodemon');
+
+var path = {
+  HTML: 'src/index.html',
+  MINIFIED_OUT: 'build.min.js',
+  OUT: 'build.js',
+  DEST: 'dist',
+  DEST_BUILD: 'dist/build',
+  DEST_SRC: 'dist/src',
+  ENTRY_POINT: './src/js/app.js'
+};
+
+gulp.task('copy', function(){
+  gulp.src(path.HTML)
+    .pipe(gulp.dest(path.DEST));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(path.HTML, ['copy']);
+
+  var watcher  = watchify(browserify({
+    entries: [path.ENTRY_POINT],
+    transform: [reactify],
+    debug: true,
+    cache: {}, packageCache: {}, fullPaths: true
+  }));
+
+  return watcher.on('update', function () {
+    watcher.bundle()
+      .pipe(source(path.OUT))
+      .pipe(gulp.dest(path.DEST_SRC))
+      console.log('Updated');
+  })
+    .bundle()
+    .pipe(source(path.OUT))
+    .pipe(gulp.dest(path.DEST_SRC));
+});
+
+gulp.task('serve', function() {
+  nodemon({
+    script: 'bin/www',
+    ext: 'js',
+  })
+});
+
+gulp.task('default', ['serve', 'watch']);
---
+++ src/js/components/TodoApp.react.js
@@ -0,0 +1,17 @@
+var React = require('react');
+
+var TodoApp = React.createClass({
+
+  /**
+   * @return {object}
+   */
+  render: function() {
+    return (
+      <div>
+        Open the door Hal!
+      </div>
+    );
+  }
+});
+
+module.exports = TodoApp;
