fbd76b50c944201e791a0b0a75271f93ae674e0d
Nicolas Santini <nicolas.santini@findly.com>
2015-05-21 21:40:20 +1200

    moving map into render return

--- src/js/components/Todos.react.js
+++ src/js/components/Todos.react.js
@@ -4,14 +4,15 @@ var _ = require("underscore");
 var Todos = React.createClass({
 
   render: function() {
-    var todos = _.map(this.props.data, function(todo) {
-      return (<li className="list-group-item">{todo.text}</li>);
-    });
 
     return (
       <div className="row">
         <ul className="list-group">
-          {todos}
+          {
+            _.map(this.props.data, function(todo) {
+              return (<li className="list-group-item">{todo.text}</li>);
+            })
+          }
         </ul>
       </div>
     );
